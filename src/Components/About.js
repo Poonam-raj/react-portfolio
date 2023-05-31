06512525cb8c22f376cd51fa28e71471e50cc411
Poonam <p_rajput@hotmail.co.uk>
2023-05-31 22:34:03 +0100

    about-me content

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,17 +1,23 @@
 const About = () => {
   return (
-    <section>
-      <div>
-        <h2>About Me</h2>
-        <p>
-          My name's Poonam (she/her) - a full-stack developer and wearer of many
-          hats. I believe tech should be people-first, equity-centred and
-          sustainably focused.
-        </p>
-      </div>
-      <div>
-        <img src="../public/images/20200919_172232.jpg" alt="Poonam Rajput" />
-      </div>
+    <section class="about-me__container">
+      <p class="about-me__information">
+        My name's Poonam (she/her) - a full-stack developer and wearer of many
+        hats. I believe tech should be people-first, equity-centred and
+        sustainably focused.
+        <br />
+        <br /> I mentor people to become fully capable programmers from no
+        coding experience. I believe in technology as a tool, humane software
+        and healthy connections to the digital world.
+      </p>
+      <p class="about-me__technologies">
+        Javascript | React | NodeJS | PSQL | Jest
+      </p>
+      <img
+        class="about-me__headshot"
+        src="../images/Poonam-profile.jpg"
+        alt="Poonam Rajput"
+      />
     </section>
   );
 };
