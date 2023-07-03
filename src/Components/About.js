a51cb644601f95c7b1b1607d5ae8a9aeafe6bbec
Poonam <p_rajput@hotmail.co.uk>
2023-07-03 16:44:55 +0100

    update about me blurb

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,13 +2,20 @@ const About = () => {
   return (
     <section className="about-me__container">
       <p className="about-me__information">
-        My name's Poonam (she/her) - a full-stack developer and wearer of many
-        hats. I believe tech should be people-first, equity-centred and
-        sustainably focused.
+        Hi 👋🏽 I'm Poonam (she/her) - a full-stack developer primarily coding in
+        JavaScript.
         <br />
-        <br /> I mentor people to become fully capable programmers from no
-        coding experience. I believe in technology as a tool, humane software
-        and healthy connections to the digital world.
+        **Career so far:** I started out coding thanks to CodeFirstGirls and
+        then Northcoders. I am currently a coding educator and advocate for tech
+        being an "all-in" industry.
+        <br />
+        **Skills:** I'm not just a diligent developer, I'm a advocate for
+        pairing and clean coding practices, a public speaker, a keen leader in
+        agile teams and a recently graduated apprentice!
+        <br />
+        **Passions:** I believe in technology as a tool, humane software and
+        healthy connections to the digital world. I believe tech should be
+        people-first, equity-centred and sustainably focused.
       </p>
       <p className="about-me__technologies">
         Javascript | React | NodeJS | PSQL | Jest
